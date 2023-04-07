18a5e22c4b6f9ceadc2661b8f24062503e186109
Sushil Dahakal <sushilldhakal25@gmail.com>
2023-04-07 14:57:47 +0545

    l

--- streamlitApp.py
+++ streamlitApp.py
@@ -139,7 +139,7 @@ ctx = webrtc_streamer(
     key="example",
     video_processor_factory=VideoProcessor,
     rtc_configuration={
-        "iceServers": [{"urls": ["stun:iphone-stun.strato-iphone.de:3478"]}]
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     },
     media_stream_constraints={
         "video" : True,
